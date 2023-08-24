e50d1fade99f9ef04c3fd59be22d8778d2e50c01
Mubaraka30 <115933417+Mubaraka30@users.noreply.github.com>
2023-08-24 14:47:55 +0530

    Add files via upload

---
+++ cc.js
@@ -0,0 +1,46 @@
+
+
+
+const populate= async(value ,currency)=>{
+  let myStr = ""
+
+  url =" https://api.currencyapi.com/v3/latest?apikey=cur_live_Cl0tm1AUaanmdnNIjBllsDNLLhU4hMrKOPytnM1X&base_currency=" + currency
+
+  let response = await fetch(url)
+  let rjson = await response.json()
+
+  document.querySelector(".output").style.display ="block" 
+  
+  for(let key of Object.keys(rjson["data"])){
+
+   myStr+= `
+      <tr>
+        <td>${key}</td>
+        <td>${rjson["data"][key]["code"]}</td>
+        <td>${Math.round(rjson["data"][key]["value"] * value)}</td>
+      </tr>
+      `
+
+  }
+
+  const tableBody = document.querySelector("tbody");
+  tableBody.innerHTML = myStr;
+ 
+  document.querySelector(".loading").style.display = "none";
+}
+
+
+const btn =document.querySelector(".btn");
+btn.addEventListener("click", (e)=>{
+  e.preventDefault()
+
+  document.querySelector(".loading").style.display = "flex";
+
+  const value = parseInt(document.querySelector("input[name = 'quantity']").value);
+  const currency = document.querySelector("select[name = 'currency']").value;
+  populate(value,currency)
+  
+})
+
+
+
